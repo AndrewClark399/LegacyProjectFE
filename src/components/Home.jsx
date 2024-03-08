be51164e40d077f1fc624982c9dfdfd562325b3a
treesacherian <treesa.cherian@gmail.com>
2024-03-08 08:19:33 +0000

    project completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,20 +15,21 @@ function Home() {
                 {/* <p>CCZone Homepage</p> */}
                 
                 <div >
+                    <p></p>
                     
                 <img className="text-center" style={{ marginLeft:"1030px", width: "10%" }} src={onLine}></img>
                     <p style={{ textAlign:"center", fontFamily:"cursive",color:"green"}}><b>The new way to buy<br></br> your groceries from the <br></br>comfort of your Sofa<br></br>
                                          </b>  </p>
                 </div>
                 <div>
-                    <img className="text-center" style={{marginLeft:"550px", width: "10%" }} src={lowPrice}></img>
+                    <img className="text-center" style={{marginLeft:"750px", width: "10%" }} src={lowPrice}></img>
                     <img className="text-center" style={{ marginLeft:"400px",width: "10%" }} src={basket}></img>
                     <p></p>
                      <p style={{ textAlign:"center", fontFamily:"cursive",color:"green"}}><b>Price matched against all major supermarkets. </b></p> 
                 </div>
 
                 <div>
-                    <img className="text-center" style={{ marginLeft:"830px",width: "10%" }} src={priceMatch}></img>
+                    <img className="text-center" style={{ marginLeft:"1100px",width: "10%" }} src={priceMatch}></img>
                     {/* <p>Here to help with the cost of living!</p> */}
                 </div>
                 <div>
